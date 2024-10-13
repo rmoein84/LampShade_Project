81ad68563abe59e5ba41960a05b1859d227d8b9e
Moein Rahimi <rmoein05@gmail.com>
2024-10-13 20:35:01 +0330

    Product Ui Implemented

--- 01_LampShade.Query/Query/ProductQuery.cs
+++ 01_LampShade.Query/Query/ProductQuery.cs
@@ -4,6 +4,7 @@ using DiscountManagement.Infrastructure.EFCore;
 using InventoryManagement.Infrasturcture.EFCore;
 using Microsoft.EntityFrameworkCore;
 using ShopManagement.Domain.ProductAgg;
+using ShopManagement.Domain.ProductPictureAgg;
 using ShopManagement.Infrastructere.EFCore;
 
 namespace _01_LampShade.Query.Query
@@ -33,14 +34,20 @@ namespace _01_LampShade.Query.Query
 
         public List<ProductQueryModel> Search(string value)
         {
-            var products = _context.Products
+            var query = _context.Products
                 .AsNoTracking()
                 .Include(x => x.Category)
-                .OrderByDescending(x => x.Id)
-                .ToList();
-
+                .OrderByDescending(x => x.Id);
+            List<Product> products;
             if (!string.IsNullOrWhiteSpace(value))
-                products = products.Where(x => x.Title.Contains(value)).ToList();
+            {
+                products = query.Where(x => x.Title.Contains(value)).ToList();
+            }
+            else
+            {
+                products = query.ToList();
+
+            }
             return MapProducts(products, _inventoryContext, _discountContext);
         }
         private static List<ProductQueryModel> MapProducts(List<Product> products,
@@ -49,9 +56,9 @@ namespace _01_LampShade.Query.Query
             var result = new List<ProductQueryModel>();
             foreach (var product in products)
             {
-                var productPrice = inventoryContext.Inventory
+                var inventory = inventoryContext.Inventory
                     .AsNoTracking()
-                    .Select(x => new { x.ProductId, x.UnitPrice })
+                    .Select(x => new { x.ProductId, x.UnitPrice, x.InStock })
                     .FirstOrDefault(x => x.ProductId == product.Id);
                 var discount = discountContext.CustomerDiscounts
                    .AsNoTracking()
@@ -68,29 +75,57 @@ namespace _01_LampShade.Query.Query
                     PictureAlt = product.PictureAlt,
                     PictureTitle = product.PictureTitle,
                     Slug = product.Slug,
+                    Code = product.Code,
+                    Description = product.Description,
+                    ShortDescription = product.ShortDescription,
+                    MetaDescription = product.MetaDescription,
+                    Keywords = product.Keywords,
                 };
-                if (productPrice != null)
+                if (inventory != null)
                 {
-                    item.Price = productPrice.UnitPrice.ToMoney();
+                    item.Price = inventory.UnitPrice.ToMoney();
+                    item.IsInStock = inventory.InStock;
                     if (discount != null)
                     {
                         item.DiscountRate = discount.DiscountRate;
                         item.DiscountExpireDate = discount.EndDate.ToString();
-                        var discountAmount = Math.Round((productPrice.UnitPrice * discount.DiscountRate) / 100);
-                        item.PriceWithDiscount = (productPrice.UnitPrice - discountAmount).ToMoney();
+                        var discountAmount = Math.Round((inventory.UnitPrice * discount.DiscountRate) / 100);
+                        item.PriceWithDiscount = (inventory.UnitPrice - discountAmount).ToMoney();
                     }
+                }
+                if (product.ProductPictures != null)
+                {
+                    item.Pictures = MapProductPictures(product.ProductPictures);
                 }
                 result.Add(item);
             }
             return result;
         }
+
+        private static List<ProductPictureQueryModel> MapProductPictures(List<ProductPicture> productPictures)
+        {
+            return productPictures
+                .Where(x=>!x.IsRemoved)
+                .Select(x=> new ProductPictureQueryModel
+                {
+                    Picture = x.Picture,
+                    PictureAlt = x.PictureAlt,
+                    PictureTitle = x.PictureTitle,
+                    ProductId = x.ProductId,
+                })
+                .ToList();
+        }
         public ProductQueryModel GetDetails(string slug)
         {
-            var products = _context.Products
+            var product = _context.Products
                 .AsNoTracking()
                 .Include(x => x.Category)
+                .Include(x=>x.ProductPictures)
                 .ToList();
-            return MapProducts(products, _inventoryContext, _discountContext)
+
+            if (product == null) return new ProductQueryModel();
+
+            return MapProducts(product, _inventoryContext, _discountContext)
                 .FirstOrDefault(x => x.Slug == slug);
         }
     }
